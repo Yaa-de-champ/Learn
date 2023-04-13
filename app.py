7e3b36719919aba7466edaac33bac86fe0f1d4f4
Nana Yaa Adomaa Doku-Amponsah <dokuamponsahnanayaaadomaa@gmail.com>
2023-04-13 17:05:27 +0000

    Classes in python
    Added a student and person class
    Uses the idea of inheritance

--- app.py
+++ app.py
@@ -12,3 +12,21 @@ def prodFunction(x, y):
 
 def powFunction(x, y):
     return x**y
+
+class Person:
+    def __init__(self, name, address, telephone_number):
+        self.name = name
+        self.address = address
+        self.telephone_number = telephone_number
+
+class Customer(Person):
+    def __init__(self, name, address, telephone_number, customer_number, mailing_list):
+        super().__init__(name, address, telephone_number)
+        self.customer_number = customer_number
+        self.mailing_list = mailing_list
+
+# person = Person('Nana Yaa', 'GE-669-88', '057-04-6841')
+customer = Customer('Janet Aboi', 'GE-099-56', '057-456-8790', '020-5164-254', True)
+
+print(customer.name)
+print(customer.mailing_list)
