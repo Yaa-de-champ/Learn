5b3467a8c0dc7a8b1da319e6ec2348e50094bfee
Nana Yaa Adomaa Doku-Amponsah <dokuamponsahnanayaaadomaa@gmail.com>
2023-08-11 22:44:50 +0000

    changes

---
+++ Learn Python/mslearn.py
@@ -0,0 +1,85 @@
+# new_planet = ''
+# planets = []
+
+# while new_planet.lower() != 'done':
+#     if new_planet:
+#         planets.append(new_planet)
+#     new_planet = input('Enter a new planet, or done if done')
+
+# print(planets)
+
+
+# planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+# print("The first planet is ", planets[0])
+# print("The second planet is ", planets[1])
+# print("The third planet is ", planets[2])
+
+# countdown = [4, 3, 2, 1, 0]
+# for number in countdown:
+#     print(number)
+# print("Blast off!! ")
+
+
+# from time import sleep
+
+# countdown = [4, 3, 2, 1, 0]
+
+# for number in countdown:
+#     print(number)
+#     sleep(1)  # Wait 1 second
+# print("Blast off!! ")
+
+
+
+# new_planet = ''
+# planets = []
+
+# while new_planet.lower() != 'done':
+#     if new_planet:
+#         planets.append(new_planet)
+#     new_planet = input('Enter a new planet, or done if done')
+
+# print(planets)
+
+# for planet in planets:
+#     print(planet)
+
+
+# x = 1
+# print(type(x))
+
+# m = 1.0
+# print(type(m))
+
+# y = True
+# print(type(y))
+
+# m = "this is a string"
+# print(m)
+# print(type(m))
+
+# print("hello" + " " + "world")
+# print("hello world")
+
+## creating a class
+
+# class Elevator:
+#     def __init__(self, starting_floor):
+#         self.make = "The elevator company"
+#         self.floor = starting_floor
+
+# # To create the object
+
+# elevator = Elevator(1)
+# print(elevator.make) # "The Elevator company"
+# print(elevator.floor) # 1
+
+# from sklearn import tree
+# features = [[140, "smooth"], [130, "smooth"], [150, "bumpy"], [170, "bumpy"] ]
+# labels = ["apple", "apple", "orange", "orange"]
+# clf = tree.DecisionTreeClassifier()
+# clf = clf.fit(features, labels)
+# print clf.predict([[150, 0]])
+
+print("hey")
