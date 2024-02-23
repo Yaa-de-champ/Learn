811b54fc2bdd181a4bdd877df647a13eda9e5b4f
Nana Yaa Adomaa Doku-Amponsah <dokuamponsahnanayaaadomaa@gmail.com>
2024-02-23 11:01:30 +0000

    creating contact class and objects

--- Learn Python/contacts_manager.py
+++ Learn Python/contacts_manager.py
@@ -21,3 +21,31 @@ def add(mycontacts):
 
 mycontacts = {}
 add(mycontacts)
+
+
+def change(mycontacts):
+    name = input("Enter a name: ")
+
+    if name in mycontacts:
+
+        phone = input("Enter the new phone number: ")
+        email = input('Enter the new email address: ')
+
+        entry = co.Contacts(name, phone, email)
+
+        mycontacts[name] = entry
+
+        print('Information updated')
+    else:
+        print('That name is not found')
+
+mycontacts = {
+    'Daniel Narh': co.Contacts('Daniel Narh', '0502317445', 'danielnarh234@gmail.com'),
+    'Calista Kunge' : co.Contacts('Calista Kunge', '0203456789', 'calsitakunge789@gmail.com')
+}
+
+
+for contact_name, contact_info in mycontacts.items():
+    print(f'{contact_name}: {contact_info.phone}, {contact_info.email}' )
+
+change(mycontacts)
